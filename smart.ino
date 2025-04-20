b54f6491d40346a6d9ca3acdd8ca7ba58829e1cd
Clyde <clydesavvy@gmail.com>
2025-04-20 20:15:06 +0800

    Apply Ai Suggestions

--- smart.ino
+++ smart.ino
@@ -4,88 +4,86 @@ JsonDocument read, write;
 String slots[8];
 int front_ir = 0;
 int exit_ir = 52;
-int slot1[1] = {22};
-int slot2[1] = {53};
-int slot3[1] = {50};
-// int slot4[1] = {28};
 
-bool slot1_parked = false;
-bool slot2_parked = false;
+struct ParkingSlot {
+	int pinStart;
+	int pinEnd;
+	bool parked;
+	int index;
+}
+
+ParkingSlot slots_info[] = {
+	{22, 0, false, 1}
+	{24, 0, false, 2}
+	{26, 0, false, 3}
+	{28, 0, false, 4}
+};
+
 bool exit_parked = false;
 
-// unsigned long frontGateOpenTime = 0;
-// unsigned long exitGateOpenTime = 0;
-
-// bool frontGateOpen = false;
-// bool exitGateOpen = false;
-
-// bool frontTriggered = false;
-// bool exitTriggered = false;
 
 void setup()
 {
-	Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(exit_ir, INPUT);
-  pinMode(slot1[0], INPUT);
-  pinMode(slot2[0], INPUT);
-  pinMode(slot3[0], INPUT);
-  // pinMode(slot4[0], INPUT);
+  
+  for (int x = 0; x < 4; x++) {
+	pinMode(slots_info[x].pinStart, INPUT);
+  }
 }
 
 void loop()
 {
   read_from_python();
- if (digitalRead(slot1[0]) == 0 && slots[0] == "Y" && !slot1_parked) {
-    slot1_parked = true;
-    write.clear();
-    write["cmd"] = "parking";
-    write["context"] = 1;
-    serializeJson(write, Serial);
-    Serial.println();
- } else if (digitalRead(slot1[0]) == 1 && slots[0] == "N" && slot1_parked) {
-    slot1_parked = false;
-    write.clear();
-    write["cmd"] = "departed";
-    write["context"] = 1;
-    serializeJson(write, Serial);
-    Serial.println();
- }
- if (digitalRead(slot2[0]) == 0 && slots[1] == "Y" && !slot2_parked) {
-    slot2_parked = true;
-    write.clear();
-    write["cmd"] = "parking";
-    write["context"] = 2;
-    serializeJson(write, Serial);
-    Serial.println();
- } else if (digitalRead(slot2[0]) == 1 && slots[1] == "N" && slot2_parked) {
-    slot2_parked = false;
-    write.clear();
-    write["cmd"] = "departed";
-    write["context"] = 2;
-    serializeJson(write, Serial);
-    Serial.println();
- }
- if (digitalRead(exit_ir) == 0 && !exit_parked) {
-    exit_parked = true;
-    write.clear();
-    write["cmd"] = "exit";
-    serializeJson(write, Serial);
-    Serial.println();
- } else if (digitalRead(exit_ir) == 1 && exit_parked) {
-    exit_parked = false;
-    write.clear();
-    write["cmd"] = "clear_toll";
-    serializeJson(write, Serial);
-    Serial.println();
- }
- delay(1500);
+  handle_sensor_reading();
+  delay(500);
+}
+
+void handle_exit_sensor() {
+	if (digitalRead(exit_ir) == 0 && !exit_parked) {
+		exit_parked = true;
+		write.clear();
+		write["cmd"] = "exit";
+		serializeJson(write, Serial);
+		Serial.println();
+	} else if (digitalRead(exit_ir) == 1 && exit_parked) {
+		exit_parked = false;
+		write.clear();
+		write["cmd"] = "clear_toll";
+		serializeJson(write, Serial);
+		Serial.println();
+	}
+}
+
+void handle_sensor_reading() {
+	for (int s = 0; s < 4; s++) {
+		ParkingSlot &slot = slots_info[s];
+		int startSensor = digitalRead(slot.pinStart);
+
+		if (startSensor == 0 && slots[s] == "Y" && !slot.parked) {
+			slot.parked = true;
+			write.clear();
+			write["cmd"] = "parking";
+			write["context"] = 1;
+			serializeJson(write, Serial);
+			Serial.println();
+		} else if (startSensor == 1 && slots[s] == "N" && slot.parked) {
+			slot.parked = false;
+			write.clear();
+			write["cmd"] = "departed";
+			write["context"] = 1;
+			serializeJson(write, Serial);
+			Serial.println();
+		}
+	}
+	handle_exit_sensor();
 }
 
 void read_from_python() {
   if (Serial.available()) {
     String input = Serial.readStringUntil('\n');
     if (input) {
-      // read.clear();
+      read.clear();
       deserializeJson(read, input);
       String command = read["cmd"];
 
